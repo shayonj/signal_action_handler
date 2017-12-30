86e0d031857450526a56ec3cbd8e5369e437c108
Shayon Mukherjee <dev@shayon.me>
2017-12-30 11:08:28 -0500

    Move RESERVED_SIGNALS away from top level constant

--- lib/signal_action_handler.rb
+++ lib/signal_action_handler.rb
@@ -1,2 +1,5 @@
-RESERVED_SIGNALS = %w[ILL FPE BUS SEGV VTALRM]
 require "signal_action_handler/signal_action_handler"
+
+class SignalActionHandler
+  RESERVED_SIGNALS = %w[ILL FPE BUS SEGV VTALRM]
+end
